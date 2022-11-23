ee5cd0039f55639dbb606ac9e5cb7bc1710b8270
Claudio Pastenes <claudio.pastenes@informaticapucv.cl>
2022-11-23 18:43:10 -0300

    aa

--- resources/views/estudiante/listar.blade.php
+++ resources/views/estudiante/listar.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="col-md-10 mt-3 row">
-        <h1>Estudiante</h1>
-        <div class="mb-3 col-6">
-            Apellido Paterno
-        </div>
-        <div class="mb-3 col-6">
-            Apellido Materno
-        </div>
-        <div class="mb-3 col-6">
-            Nombres
-        </div>
-        <div class="mb-3 col-6">
-            RUN
-        </div>
-        <div class="mb-3 col-6">
-            Nivel
-        </div>
-        <div class="mb-3 col-6">
-            Prioridad
-        </div>
-        <h1>Apoderado</h1>
-        <div class="mb-3 col-6">
-            Nombre completo
-        </div>
-        <div class="mb-3 col-6">
-            Teléfono
-        </div>
-        <div class="mb-3 col-6">
-            Correo Electrónico
-        </div>
+    <div style="background-color: #fff; border: solid black 1px; border-radius: 1rem">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">RUN</th>
+                <th scope="col">Prioridad</th>
+                <th scope="col">Ver</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>11111111-1</td>
+              </tr>
+              <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>11111111-1</td>
+              </tr>
+              <tr>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>11111111-1</td>
+              </tr>
+            </tbody>
+          </table>
     </div>
+
 </div>
 @endsection
