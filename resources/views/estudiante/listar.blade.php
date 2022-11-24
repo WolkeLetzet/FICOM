f03cf06330b8fde2c44f04158aee009c36737ea7
Aksel <ormenhoaxel@gmail.com>
2022-11-23 23:46:13 -0300

    Show estudiante

--- resources/views/estudiante/listar.blade.php
+++ resources/views/estudiante/listar.blade.php
@@ -14,50 +14,51 @@
             </thead>
             <tbody>
               @foreach ($estudiantes as $estud)
-                
-              
               <tr 
-              @if ($estud->prioridad == 1)
-                class="table-light"
-              @elseif ($estud->prioridad == 2)
-                class="table-danger"
-              @elseif ($estud->prioridad == 3)
-                class="table-primary"
-              @endif>
+                @switch ($estud->prioridad)
+                  @case(1)
+                    class="table-light"
+                    @break
+                  
+                  @case(2)
+                    class="table-danger"
+                    @break
+                  
+                  @case(3)
+                    class="table-primary"
+                    @break
+                @endswitch
+              >
                 <td>{{$estud->nombres}}</td>
                 <td>{{$estud->apellidos}}</td>
                 <td>{{$estud->rut}}</td>
-                <td >
-                  @if ($estud->prioridad == 1)
-                    Sin Beneficios
-                  @elseif ($estud->prioridad == 2)
-                    Nuevo Prioritario
-                  @elseif ($estud->prioridad == 3)
-                    Prioritario
-                  @endif
+                <td>
+                  @switch ($estud->prioridad)
+                    @case(1)
+                      Sin Beneficios
+                      @break
+                    
+                    @case(2)
+                      Nuevo Prioritario
+                      @break
+                    
+                    @case(3)
+                      Prioritario
+                      @break
+                  @endswitch
+                </td>
+                <td>
+                  <a href="estudiante/{{$estud->id}}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                      <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                      <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                    </svg>
+                  </a>
                 </td>
               </tr>
               @endforeach
             </tbody>
           </table>
     </div>
-
-    <div class="container">
-      <div class="grid">
-        <div class="row">
-          <div class="col">
-            <pre>
-            {{json_encode($estudiantes , JSON_PRETTY_PRINT); }}
-            </pre>
-          </div>
-          <div class="col">
-            <pre>
-              {{json_encode($apoderados , JSON_PRETTY_PRINT); }}
-            </pre>
-          </div>
-        </div>
-      </div>
-    </div>
-
 </div>
 @endsection
--- resources/views/estudiante/perfil.blade.php
+++ resources/views/estudiante/perfil.blade.php
@@ -1,49 +1,50 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="container">
     <form class="col-md-10 mt-3 row">
         <h1>Estudiante</h1>
-        <div class="form-group mb-3 col-3">
-            <label for="apellido_paterno" class="form-label">Apellido Paterno</label>
-            <input type="text" name="apellido_paterno" class="form-control" autofocus>
+        <div class="form-group mb-3 col-4">
+            <label for="apellido_paterno" class="form-label">Apellidos</label>
+            <input type="text" name="apellido_paterno" class="form-control" value="{{$estudiante->apellidos}}" disabled>
         </div>
-        <div class="form-group mb-3 col-3">
-            <label for="apellido_materno" class="form-label">Apellido Materno</label>
-            <input type="text" name="apellido_materno" class="form-control">
-        </div>
-        <div class="form-group mb-3 col-6">
+        <div class="form-group mb-3 col-4">
             <label for="nombres" class="form-label">Nombres</label>
-            <input type="text" name="nombres" class="form-control">
+            <input type="text" name="nombres" class="form-control" value="{{$estudiante->nombres}}" disabled>
         </div>
         <div class="form-group mb-3 col-4">
             <label for="run" class="form-label">RUN</label>
-            <input type="text" name="run" class="form-control">
+            <input type="text" name="run" class="form-control" value="{{$estudiante->rut}}" disabled>
+        </div>
+        <div class="form-group mb-3 col-4">
+            <label for="email" class="form-label">Correo Electrónico</label>
+            <input type="text" name="email" class="form-control" value="{{$estudiante->email_institucional}}" disabled>
         </div>
         <div class="form-group mb-3 col-4">
             <label for="nivel" class="form-label">Nivel</label>
-            <input type="text" name="nivel" class="form-control">
+            <input type="text" name="nivel" class="form-control" value="{{$estudiante->curso_id}}" disabled>
         </div>
         <div class="form-group mb-3 col-4">
             <label for="prioridad" class="form-label">Prioridad</label>
-            <select name="prioridad" class="form-control">
+            <select name="prioridad" class="form-control" disabled>
                 <option value="" selected disabled hidden>Selecciona una opción</option>
-                <option value="">No proritario</option>
-                <option value="">Nuevo proritario</option>
-                <option value="">Proritario</option>
+                <option value="1" @if($estudiante->prioridad == 1) selected @endif>No proritario</option>
+                <option value="2" @if($estudiante->prioridad == 2) selected @endif>Nuevo proritario</option>
+                <option value="3" @if($estudiante->prioridad == 3) selected @endif>Proritario</option>
             </select>
         </div>
         <h1>Apoderado</h1>
         <div class="form-group mb-3 col-6">
             <label for="fullname" class="form-label">Nombre completo</label>
-            <input type="text" name="fullname" class="form-control">
+            <input type="text" name="fullname" class="form-control" value="{{$estudiante->nombres . ' ' . $estudiante->apellidos}}" disabled>
         </div>
         <div class="form-group mb-3 col-6">
             <label for="telefono" class="form-label">Teléfono</label>
-            <input type="text" name="telefono" class="form-control">
+            <input type="text" name="telefono" class="form-control" value="{{$estudiante->telefono}}" disabled>
         </div>
         <div class="form-group mb-3 col-6">
             <label for="email" class="form-label">Correo Electrónico</label>
-            <input type="text" name="email" class="form-control">
+            <input type="text" name="email" class="form-control" value="{{$estudiante->email}}" disabled>
         </div>
     </form>
 </div>
