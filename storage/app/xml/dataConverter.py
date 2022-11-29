3d0a81e04cfcbcbc3c2e12bc234a5ed1f6ae660d
Claudio Pastenes <claudio.pastenes@informaticapucv.cl>
2022-11-29 14:07:21 -0300

    run cache

--- storage/app/xml/dataConverter.py
+++ storage/app/xml/dataConverter.py
@@ -6,15 +6,11 @@ import csv
 
 path=os.path.dirname(os.path.realpath(__file__))
 xml_path = sys.argv[1]
-json_path = path+"/nomina.json"
 with open(xml_path,'r',encoding="utf-8-sig",errors='replace') as fp:
    xml = fp.read()
    dictionary = xmltodict.parse(xml)
    fp.close()
 
-with open(json_path,"w",encoding="utf-8-sig") as of:
-   json.dump(dictionary,of,indent=3) 
-   fp.close()
  
 
    
